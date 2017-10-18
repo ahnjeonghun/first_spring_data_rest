ec7dc6c1059a2366b19f8f6ac6202794dfa28db1
aslan <an86baby@gmail.com>
2017-10-19 08:46:23 +0900

    Initial commit

---
+++ src/main/resources/initialize_queries.sql
@@ -0,0 +1,3 @@
+insert into person (first_name,last_name) VALUES ('서울','안정훈');
+insert into person (first_name,last_name) VALUES ('서울','배창현');
+insert into person (first_name,last_name) VALUES ('서울','김보훈');
